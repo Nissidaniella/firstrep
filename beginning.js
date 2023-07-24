5e174d1ec305016ee54840f63a57a96a01ddf47f
Nissi Daniella Diallo <opongdiallonissi@gmail.com>
2023-07-24 14:06:22 +0000

    first commit

---
+++ beginning.js
@@ -0,0 +1,17 @@
+// var you = prompt('Hello! What is your name?');
+// var up = (you= you.toUpperCase(), you.slice(0, 1));
+// var low = (you = you.toLowerCase(), you.slice(1, 10));
+// alert ('Nice to meet you ' + up + low);
+
+// var dogA = prompt ('what is your dogs age?');
+// var humA = ((dogA - 2) * 4 )+ 21;
+// alert ('your dog is ' + humA + ' years old in human years');
+
+//  var you = prompt('Hello! What is your name?');
+// var  slice = you.slice(0, 5);
+// alert (slice);
+
+// var you = prompt('Hello! What is your name?');
+// alert ('You have typed ' + you.length + ' words and you have '  + (10 - you.length) + ' more words remaining');
+
+console.log("hello nii");
